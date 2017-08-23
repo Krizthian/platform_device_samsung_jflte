dbcc3a4200f508f261aa17c8b041125cec9da450
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-23 20:12:16 +0200

    Camera: build with clang
    Also remove duplicated flag

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,12 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_CLANG := false
-
 LOCAL_C_INCLUDES += \
     system/media/camera/include
-
-LOCAL_CLANG := false
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
