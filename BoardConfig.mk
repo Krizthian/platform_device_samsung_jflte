d81602795ba1424ebf2e93f71a26e433be6ab60b
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-24 01:37:56 +0200

    Sepolicy: temp drop QCOM and specific device sepolicy rules
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,8 +132,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 BOARD_RIL_CLASS := ../../../$(COMMON_PATH)/ril
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_jflte
