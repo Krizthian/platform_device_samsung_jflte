0a558af64b2ad9ba4dd998a71f133a882f80d65e
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-10 14:33:17 +0200

    jf: set BTIF_HF_WBS_PREFERRED to false

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,6 +24,6 @@
 #define BTA_DISABLE_DELAY 500 /* in milliseconds */
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+#define BTIF_HF_WBS_PREFERRED FALSE  /* Do not use WBS    */
 
 #endif
